46c73f52003ffb91764e6b57e1ea66f645e0c397
gowthami91 <141713796+gowthami91@users.noreply.github.com>
2025-09-23 09:09:18 +0530

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,18 @@
+package org;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Registration
+ */
+@WebServlet("/register")
+public class Registration extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
